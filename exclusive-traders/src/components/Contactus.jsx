156e373070eff10dc0cf96ebf5a4894a50e74f0c
AtirathTraders <madhapuroffice9@gmail.com>
2026-02-13 15:44:15 +0530

    updated

--- exclusive-traders/src/components/Contactus.jsx
+++ exclusive-traders/src/components/Contactus.jsx
@@ -66,9 +66,8 @@ const Contact = ({ onBackToHome }) => {
                   <h3 className="font-semibold text-white mb-2">
                     Our Office
                   </h3>
-                  <p className="text-light/70">1st Floor, 8 Quary Wharf, Abbey Road,
-                    Barking, London, IG11 7BZ.</p>
-                  <p className="text-light/70"></p>
+                  <p className="text-light/70">123 Trade Center</p>
+                  <p className="text-light/70">Mumbai, India 400001</p>
                 </div>
               </div>
 
@@ -81,7 +80,7 @@ const Contact = ({ onBackToHome }) => {
                   <h3 className="font-semibold text-white mb-2">
                     Call Us
                   </h3>
-                  <p className="text-light/70">+44 20 1234 5678</p>
+                  <p className="text-light/70">+91 59765 43210</p>
                 </div>
               </div>
 
@@ -94,7 +93,7 @@ const Contact = ({ onBackToHome }) => {
                   <h3 className="font-semibold text-white mb-2">
                     Email
                   </h3>
-                  <p className="text-light/70">fmcg@exclusivetrader.co.uk</p>
+                  <p className="text-light/70">contact@exclusivetrader.com</p>
                 </div>
               </div>
 
